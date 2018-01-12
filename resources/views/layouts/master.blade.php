849f0f70bd134c06e920ba39ff9b15b26a0daeb0
Leon van der Houven <leon.metal.1994@live.nl>
2018-01-12 22:35:15 +0100

    scipt

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,8 @@
 	<title>Leon van der Houven</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
 	<link rel="stylesheet" href="{{secure_asset('css/app.css')}}">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
+	"></script>
 	<script src="{{secure_asset('js/functions.js')}}"></script>
 
 </head>
